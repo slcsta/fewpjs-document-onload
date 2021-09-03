e3127a3776742b37b94f93d2190de635e3675041
Sarah <sarahlchavez@gmail.com>
2021-09-03 05:16:29 -0700

    commit tree 00dcb48a1757c8466eac322c447ca392a2053934

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+// Add a DOMContentLoaded event listener to detect when HTML has loaded
+// Use the event's callback function to identify paragraph element id="text"
+// Replace id="text" with "This is really cool!"
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("text").innerHTML = "This is really cool!";
+    // console.log("This is really cool!")
+});
+console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  );
